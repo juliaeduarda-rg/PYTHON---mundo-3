5aaef4eab738e34fd633c6be7b2c49827e6c93aa
Júlia <78328012+juliaeduarda-rg@users.noreply.github.com>
2021-06-14 08:02:41 -0300

    adicionando o que faltava
    coloquei a variável extra, que dá a possibilidade de verificar se o número estava lá antes.

--- exercicio 079.py
+++ exercicio 079.py
@@ -2,5 +2,9 @@ resp = 'S'
 num = list()
 while 'S' in resp:
     num.append(input('Escreva um número:'))
+    num.append(n)
+    elif n in num:
+        print('Já existe esse número aqui, porfavor escreva outro número')
     resp = str(input('Vcoê deseja continuar colocando números?').strip().upper())
-print(num)
+num.sort()
+print(f'Os números escritos em ordem crescente foram:{num}')
