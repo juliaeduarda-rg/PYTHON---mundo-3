1ce4ba84fc4c03f2dd0c3242bc955cbd6e97571a
Júlia <78328012+juliaeduarda-rg@users.noreply.github.com>
2021-06-14 07:38:32 -0300

    mudanças pra ficar mais simples
    colocar todas as posições dos números e não repetindo o input.

--- exercicio 078.py
+++ exercicio 078.py
@@ -1,4 +1,16 @@
-listatop = [int(input('escreve um número:')), int(input('escreve um número:')), int(input('escreve um número:')), int(input('escreve um número:')),
-int(input('escreve um número:'))]
-print(f'Seu maior número foi {max(listatop)} e ele está na posição {listatop.index(max(listatop))+1}')
-print(f'Seu menor número foi {min(listatop)} e ele está na posição {listatop.index(min(listatop))+1}')
+listatop = []
+for i in range(0,5):
+    listatop.append(int(input('Escreva um número:')))
+print(f'Seu maior número foi {max(listatop)} ')
+print(f'Seu menor número foi {min(listatop)} ')
+mx = max(listatop)
+mn= min(listatop)
+print('O meior número está na posição ', end=' ')
+for c, v in enumerate(listatop):
+    if v == mx:
+        print(f'{c+1}', end=' ')
+print(' ')
+print('O menor número está na posição ', end= ' ')
+for c, v in enumerate(listatop):
+    if v == mn:
+        print(f'{c+1}', end=' ')
